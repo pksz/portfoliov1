e9a1100d3d016762948f9d83797ecfa158216112
pankaj sharma <2000pks@gmail.com>
2025-02-18 16:32:13 +0530

    added project section

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from . import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portfolio/',include('portfolio_app.urls')),
     path('',include('portfolio_app.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/portfolio_app/models.py
+++ portfolio/portfolio_app/models.py
@@ -1,15 +1,36 @@
 from django.db import models
-import PIL
+from PIL import Image
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
+import os
 # Create your models here.
 
 class ProjectDetails(models.Model):
     project_name=models.CharField(max_length=50) 
-    project_description=models.TextField(max_length=1000) #this goes in the detail view
-    project_summary=models.CharField(max_length=100) #this goes in the main page
-    project_img=models.ImageField(blank=True,null=True)
+    project_description=models.TextField(max_length=10000) #this goes in the detail view
+    project_summary=models.CharField(max_length=300) #this goes in the main page
+    project_img=models.ImageField(blank=True,null=True,upload_to='project_images/',default='project_images/default.webp')
     
     #add a way to sort them by assigning a priority to each item
     priority=models.PositiveSmallIntegerField(blank=True,null=True)
 
     def __str__(self):
         return self.project_name
+    
+    #resize the image
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.project_img:
+            img_path = self.project_img.path
+            img = Image.open(img_path)
+            if img.height > 400 or img.width > 300:
+                output_size = (400, 300)
+                img.thumbnail(output_size, Image.LANCZOS)
+                img.save(img_path)
+
+# Signal to delete the image file when the project is deleted
+@receiver(post_delete, sender=ProjectDetails)
+def delete_project_img(sender, instance, **kwargs):
+    if instance.project_img and instance.project_img.name!='project_images/default.webp':
+        if os.path.isfile(instance.project_img.path) :
+            os.remove(instance.project_img.path)
