858b8b53506a962c7da25fb763e89dc55ac10107
pankaj sharma <2000pks@gmail.com>
2025-02-21 19:59:55 +0530

    collected staticfiles

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from . import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('portfolio/',include('portfolio_app.urls')),
     path('',include('portfolio_app.urls')),
 ]
